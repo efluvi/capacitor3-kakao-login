531a8171480761f3b5b4d9059ff47dace7ac325f
동환 <bdhwan@gmail.com>
2022-10-16 22:25:11 +0900

    fix

--- ios/Plugin/Capacitor3KakaoLogin.swift
+++ ios/Plugin/Capacitor3KakaoLogin.swift
@@ -100,21 +100,46 @@ import KakaoSDKTemplate
                                 link: link)
         let feedTemplate = FeedTemplate(content: content, social: nil, buttons: [button])
 
-        //메시지 템플릿 encode
+//        //메시지 템플릿 encode
+//        if let feedTemplateJsonData = (try? SdkJSONEncoder.custom.encode(feedTemplate)) {
+//
+//        //생성한 메시지 템플릿 객체를 jsonObject로 변환
+//            if let templateJsonObject = SdkUtils.toJsonObject(feedTemplateJsonData) {
+//                ShareApi.shared.shareDefault(templateObject:templateJsonObject) {(linkResult, error) in
+//                    if let error = error {
+//                        print(error)
+//                        call.reject("error")
+//                    }
+//                    else {
+//
+//                        //do something
+//                        guard let linkResult = linkResult else { return }
+//                        UIApplication.shared.open(linkResult.url, options: [:], completionHandler: nil)
+//
+//                        call.resolve([
+//                            "value": "done"
+//                        ])
+//                    }
+//                }
+//            }
+//
+//        }
+        
+        
         if let feedTemplateJsonData = (try? SdkJSONEncoder.custom.encode(feedTemplate)) {
 
-        //생성한 메시지 템플릿 객체를 jsonObject로 변환
+        //jsonObject
             if let templateJsonObject = SdkUtils.toJsonObject(feedTemplateJsonData) {
-                ShareApi.shared.shareDefault(templateObject:templateJsonObject) {(linkResult, error) in
+                ShareApi.shared.shareDefault(templateObject:templateJsonObject) { [weak self] (sharingResult, error) in
                     if let error = error {
                         print(error)
                         call.reject("error")
                     }
                     else {
-
                         //do something
-                        guard let linkResult = linkResult else { return }
-                        UIApplication.shared.open(linkResult.url, options: [:], completionHandler: nil)
+                        guard let sharingResult = sharingResult else { return }
+                        UIApplication.shared.open(sharingResult.url, options: [:], completionHandler: nil)
+                    
                         
                         call.resolve([
                             "value": "done"
@@ -123,5 +148,8 @@ import KakaoSDKTemplate
                 }
             }
         }
+        
+        
+        
     }
 }
